881462325f781aa53154161beedc767cc9db1759
binhvp <46177935+binhvp@users.noreply.github.com>
2021-01-09 16:04:50 +0700

    Add files via upload

---
+++ test.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ * @author vophuongbinh
+ */
+
+public class test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hello Java!");
+        List l = new ArrayList();
+        l.add("A");
+        l.add(1);
+
+    }
+    
+}
